1789e325a37ef6ed2a0c308ced18e200fb82111f
Matthew Perry <perrym5@rpi.edu>
2012-04-25 23:08:44 -0400

    Now requires CELT 0.7 instead of 0.11

--- lib/celt-ruby.rb
+++ lib/celt-ruby.rb
@@ -19,14 +19,9 @@ module Celt
     CELT_GET_MODE_REQUEST           = 1
     CELT_SET_COMPLEXITY_REQUEST     = 2
     CELT_SET_PREDICTION_REQUEST     = 4
-    CELT_SET_BITRATE_REQUEST        = 6
+    CELT_SET_VBR_RATE_REQUEST       = 6
     CELT_RESET_STATE_REQUEST        = 8
-    CELT_SET_VBR_CONSTRAINT_REQUEST = 10
-    CELT_SET_VBR_REQUEST            = 12
-    CELT_SET_INPUT_CLIPPING_REQUEST = 14
-    CELT_SET_START_BAND_REQUEST     = 10000
-    CELT_SET_END_BAND_REQUEST       = 10001
-    CELT_SET_CHANNEL_REQUEST        = 10002
+    CELT_GET_FRAME_SIZE             = 1000
     CELT_GET_LOOKAHEAD              = 1001
     CELT_GET_SAMPLE_RATE            = 1003
     CELT_GET_BITSTREAM_VERSION      = 2000
@@ -36,26 +31,16 @@ module Celt
   attach_function :celt_mode_destroy, [:pointer], :void
   attach_function :celt_mode_info, [:pointer, :int, :pointer], :int
 
-  attach_function :celt_encoder_get_size, [:int], :int
-  attach_function :celt_encoder_get_size_custom, [:pointer, :int], :int
-  attach_function :celt_encoder_create, [:int, :int, :pointer], :pointer
-  attach_function :celt_encoder_create_custom, [:pointer, :int, :pointer], :pointer
-  attach_function :celt_encoder_init, [:pointer, :int, :int, :pointer], :pointer
-  attach_function :celt_encoder_init_custom, [:pointer, :pointer, :int, :pointer], :pointer
+  attach_function :celt_encoder_create, [:pointer, :int, :pointer], :pointer
   attach_function :celt_encoder_destroy, [:pointer], :void
-  attach_function :celt_encode_float, [:pointer, :pointer, :int, :pointer, :int], :int
-  attach_function :celt_encode, [:pointer, :pointer, :int, :pointer, :int], :int
+  attach_function :celt_encode_float, [:pointer, :pointer, :pointer, :pointer, :int], :int
+  attach_function :celt_encode, [:pointer, :pointer, :pointer, :pointer, :int], :int
   attach_function :celt_encoder_ctl, [:pointer, :int, :varargs], :int
 
-  attach_function :celt_decoder_get_size, [:int], :int
-  attach_function :celt_decoder_get_size_custom, [:pointer, :int], :int
-  attach_function :celt_decoder_create, [:int, :int, :pointer], :pointer
-  attach_function :celt_decoder_create_custom, [:pointer, :int, :pointer], :pointer
-  attach_function :celt_decoder_init, [:pointer, :int, :int, :pointer], :pointer
-  attach_function :celt_decoder_init_custom, [:pointer, :pointer, :int, :pointer], :pointer
+  attach_function :celt_decoder_create, [:pointer, :int, :pointer], :pointer
   attach_function :celt_decoder_destroy, [:pointer], :void
-  attach_function :celt_decode_float, [:pointer, :pointer, :int, :pointer, :int], :int
-  attach_function :celt_decode, [:pointer, :pointer, :int, :pointer, :int], :int
+  attach_function :celt_decode_float, [:pointer, :pointer, :int, :pointer], :int
+  attach_function :celt_decode, [:pointer, :pointer, :int, :pointer], :int
   attach_function :celt_decoder_ctl, [:pointer, :int, :varargs], :int
 
   attach_function :celt_strerror, [:int], :pointer
