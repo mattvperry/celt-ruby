f6d1de55768f8e78a3e4fcb6f6fdba72fac85c9d
loscoala <loscoala@users.noreply.github.com>
2015-01-17 20:19:07 +0100

    encoder is now using a single allocated buffer

--- lib/celt-ruby/encoder.rb
+++ lib/celt-ruby/encoder.rb
@@ -3,16 +3,20 @@ module Celt
     attr_reader :sample_rate, :frame_size, :channels,
                 :prediction_request, :vbr_rate
 
-    def initialize(sample_rate, frame_size, channels)
+    def initialize(sample_rate, frame_size, channels, size)
       @sample_rate = sample_rate
       @frame_size = frame_size
       @channels = channels
-
+      @size = size
+      @out = FFI::MemoryPointer.new :char, @size + 1
+      @buf = FFI::MemoryPointer.new :char, @size + 1
       @mode = Celt.celt_mode_create sample_rate, frame_size, nil
       @encoder = Celt.celt_encoder_create @mode, channels, nil
     end
 
     def destroy
+      @out.free
+      @buf.free
       Celt.celt_encoder_destroy @encoder
       Celt.celt_mode_destroy @mode
     end
@@ -37,12 +41,10 @@ module Celt
       Celt.celt_encoder_ctl @encoder, Celt::Constants::CELT_SET_VBR_RATE_REQUEST, :pointer, v_ptr
     end
 
-    def encode(data, size)
-      out = FFI::MemoryPointer.new :char, data.size + 1
-      buf = FFI::MemoryPointer.new :char, data.size + 1
-      buf.put_string 0, data
-      len = Celt.celt_encode @encoder, buf, nil, out, size
-      out.read_string_length len
+    def encode(data)
+      @buf.put_string 0, data
+      len = Celt.celt_encode @encoder, @buf, nil, @out, @size
+      @out.read_string len
     end
   end
 end
